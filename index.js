e3381942319a91772f61aa99c1b27c78cc3c2e23
Ubuntu16 <sr.front.dev.km@gmail>
2020-07-20 04:00:12 -0700

    Updated

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 2000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! Now OK'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
