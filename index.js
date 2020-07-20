85051ef7b862750cd11267c4609bb9d9df229fce
Ubuntu16 <sr.front.dev.km@gmail>
2020-07-20 04:13:11 -0700

    Akki commit msg 2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 2002
 
-app.get('/', (req, res) => res.send('Hello Akki!! How are you?'))
+app.get('/', (req, res) => res.send('Hello Akki!! How are you? Welcome home'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
