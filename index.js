5972eca4cf1d944b5c2f30a7ce705ff72a8598cc
Your Name <you@example.com>
2020-08-13 01:24:30 -0700

    Added env var

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 2002
+const myvar = process.env.myvar || 'default';
 
-app.get('/', (req, res) => res.send('Hello Akki!! How are you? Welcome home'))
+app.get('/', (req, res) => res.send('Hello Akki!! How are you? Welcome home ${myvar}'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
