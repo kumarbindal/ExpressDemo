1c30099650c40dd0a6cbb470361440a06563f274
Your Name <you@example.com>
2020-08-24 08:08:55 -0700

    Kill Process

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 2002
 const myvar = process.env.myvar || 'default';
+const killProcess = process.env.killProcess || false;
+
+process.on('SIGHUP', () => {
+  console.log('Got SIGHUP signal.');
+});
+
+if (killProcess === true) {
+  process.kill(process.pid, 'SIGHUP');
+}
 
 app.get('/', (req, res) => res.send(`Hello Akki!! How are you? Welcome home ${myvar}`))
 
